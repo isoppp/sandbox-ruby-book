97e32f9ce4b4d3e7e121e78c259845f7b0300af5
isoppp <prog@d2deck.com>
2018-09-28 13:59:09 +0900

    add Test and class

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,7 +1,17 @@
-require './lib/gate'
+require './lib/Gate'
+require './lib/Ticket'
 
-RSpec.describe 'Gate Test' do
+RSpec.describe 'Gate Test /' do
   example 'create class' do
-    expect(Gate.new).to_not eq nil
+    expect(Gate.new(:some)).to_not eq nil
+  end
+
+  example 'umeda to juso' do
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    expect(juso.exit(ticket)).to eq true
   end
 end
