c7b962b5301470208b6391f5e8820bde6d4070ac
isoppp <prog@d2deck.com>
2018-09-28 14:18:38 +0900

    add another test and Gate/Ticket class

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -6,12 +6,21 @@ RSpec.describe 'Gate Test /' do
     expect(Gate.new(:some)).to_not eq nil
   end
 
-  example 'umeda to juso' do
+  example 'umeda to juso should be able to exit' do
     umeda = Gate.new(:umeda)
     juso = Gate.new(:juso)
 
     ticket = Ticket.new(150)
     umeda.enter(ticket)
     expect(juso.exit(ticket)).to eq true
+  end
+
+  example 'umeda to mikuni should be not able to exit' do
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    expect(mikuni.exit(ticket)).to eq false
   end
 end
