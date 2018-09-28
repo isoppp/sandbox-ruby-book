99484a341f9678759cc417b84a58d03f8159946c
isoppp <prog@d2deck.com>
2018-09-28 15:22:26 +0900

    add class details

--- study/7_class.rb
+++ study/7_class.rb
@@ -5,6 +5,7 @@ class ClassName
   attr_writer :name_w
   attr_accessor :name_rw
   CLASS_CONST_VALUE = 0
+  CLASS_FREEZE_VALUE = 'string'.freeze
 
   def initialize(name = 'name')
     puts 'init'
@@ -42,6 +43,14 @@ class ClassName
 
   p 'can exec code here'
 end
+
+p '---'
+p ClassName::CLASS_CONST_VALUE #=> 0
+ClassName::CLASS_CONST_VALUE = 1 # output warning but can set new value
+p ClassName::CLASS_CONST_VALUE #=> 1
+# p ClassName::CLASS_FREEZE_VALUE #=> 0 # why don't work? maybe shouldn't be able to change
+# ClassName::CLASS_FREEZE_VALUE = 'freeze?'
+# p ClassName::CLASS_FREEZE_VALUE #=> 0
 
 c = ClassName.new
 c.hello_r
@@ -93,3 +102,83 @@ ddd = Dvd.new('super computer', '$1000', '60m')
 
 ppp.say_info
 ddd.say_info
+
+class PublicLevel
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def public_m
+    p @name
+  end
+
+  private # can't specify receiver
+
+  def private_m
+    p @name
+  end
+
+  protected
+
+  def protected_m
+    p @name
+  end
+end
+
+class PublicLevelSub < PublicLevel
+  def can_call_private_in_sub
+    private_m
+  end
+end
+
+pub = PublicLevel.new('text')
+pub.public_m
+# pub.protected_m # error
+# pub.private_m #error
+
+pub_sub = PublicLevelSub.new('text sub')
+pub_sub.can_call_private_in_sub
+
+class ManyVariableType
+  @name = 'Class Instance Variable'
+  @@name = 'Share Sub Class Class variable'
+
+  def self.name
+    @name
+  end
+
+  def self.name2
+    @@name
+  end
+
+  def initialize(str)
+    @name = str
+  end
+
+  def get_name
+    @name
+  end
+end
+
+$GLOBAL_VALUE = 'GLOBAL'
+class ManyVariableTypeSub < ManyVariableType
+  def global
+    p $GLOBAL_VALUE
+  end
+end
+
+
+m = ManyVariableType.new('string')
+
+p ManyVariableType.name
+p m.get_name
+
+ms = ManyVariableTypeSub.new('string sub')
+p ManyVariableTypeSub.name # nil don't share super to sub class (@name)
+p ManyVariableTypeSub.name2 # share super to sub class (@@name)
+p ms.get_name # string sub
+
+ms.global
+
