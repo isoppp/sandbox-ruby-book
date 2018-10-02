a1669efa9a25692370e1f07f05de5924372dd755
isoppp <prog@d2deck.com>
2018-10-02 23:58:00 +0900

    add exeption basic

---
+++ study/9_exeption.rb
@@ -0,0 +1,46 @@
+# chokidar "**/*.rb" -c "bundle exec ruby study/9_exeption.rb"
+
+module Greeter
+
+end
+
+begin
+  greeter = Greeter.new
+rescue NoMethodError => e
+  puts 'catch no method error'
+  puts "error class is #{e.class}"
+  puts "error message is #{e.message}"
+  puts "backtrace is #{e.backtrace}"
+rescue => e
+  puts "error class is #{e.class}"
+  puts "error message is #{e.message}"
+  puts "backtrace is #{e.backtrace}"
+end
+
+p 'hoge'
+
+begin
+  Greeter.no_method
+rescue NameError
+  # can call
+rescue NoMethodError
+  # never call here
+  # because NoMethodError NameError < StandardError < Exeption class tree
+  # and, catch NoMethod error include NameError
+end
+
+
+retry_count = 0
+begin
+  puts 'start progream...'
+
+  1 / 0 # exec error
+rescue
+  retry_count += 1
+  if retry_count <= 3
+    puts "retry total: #{retry_count}"
+    retry
+  else
+    puts 'failed retry X<'
+  end
+end
