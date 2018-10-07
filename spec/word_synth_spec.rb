02ab27f46b6a122fdb44b585c21d5c8268e8d766
isoppp <prog@d2deck.com>
2018-10-07 17:05:45 +0900

    Add wordsynth test

--- spec/word_synth_spec.rb
+++ spec/word_synth_spec.rb
@@ -1,8 +1,7 @@
 require './lib/word_synth'
 
-RSpec.describe 'Word Synth test' do
-  example "should word synth class and effect module exist" do
-    expect(WordSynth).not_to eq nil
+RSpec.describe 'Effects Test' do
+  example "should exist Effects module" do
     expect(Effects).not_to eq nil
   end
 
@@ -25,5 +24,30 @@ RSpec.describe 'Word Synth test' do
 
     effect = Effects.loud(3)
     expect(effect.call('Ruby is fun!')).to eq 'RUBY!!! IS!!! FUN!!!!'
+  end
+end
+
+RSpec.describe 'Word Synth Test' do
+  example 'should exist WordSynth class' do
+    expect(WordSynth).not_to eq nil
+  end
+
+  example "should be return original word" do
+    synth = WordSynth.new
+    expect(synth.play('Ruby is fun!')).to eq 'Ruby is fun!'
+  end
+
+  example "play reverse should be reverse" do
+    synth = WordSynth.new
+    synth.add_effect(Effects.reverse)
+    expect(synth.play('Ruby is fun!')).to eq 'ybuR si !nuf'
+  end
+
+  example 'mix effect' do
+    synth = WordSynth.new
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+    expect(synth.play('Ruby is fun!')).to eq '!!!YYBBUURR !!!SSII !!!!!NNUUFF'
   end
 end
